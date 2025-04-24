0536bb7cf08f2272653efdd38f1fbe1c4728daaa
Pavel.K144 <portex144@gmail.com>
2025-04-25 00:55:01 +0200

    CrouchAndSprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,70 @@
+using System;
 using UnityEngine;
 
 
-[System.Serializable]
-public struct Weapon
-{
-    public Mesh mesh;
-    public Material material;
-}
-
 public class PlayerMovement : MonoBehaviour
 {
-    public Weapon[] weapons;
-    public int diegoIndex;
-    public GameObject sword;
 
     [Header("Movement")]
-    public float speed;
+
+    private float speed;
+
+    public float walkSpeed;
+    public float sprintSpeed;
     public float groundDrag;
+
+    bool sprint;
+
+    [Header("Jumping")]
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    bool jumping;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    bool crouch;
 
     [Header("Ground Check")]
 
     public float playerHeight;
-
     public bool grounded; 
-
     public LayerMask whatIsGround;
 
     public Transform orientation;
 
-    float horizontalInput, verticalInput; // Variables para almacenar la entrada del usuario en los ejes X e Y
+    float horizontalInput;
+    float verticalInput;
 
-    Vector3 moveDirection; // Variable para almacenar la direcci�n de movimiento
+    Vector3 moveDirection;
 
-    Rigidbody rb; 
+    Rigidbody rb;
+
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         rb = GetComponent<Rigidbody>(); 
         rb.freezeRotation = true; // Congela la rotaci�n del Rigidbody para evitar que se voltee
+
+        readyToJump = true; // Inicializa el estado de salto como listo
+        startYScale = transform.localScale.y; // Guarda la escala Y inicial del objeto para poder restaurarla al salir de la posicion de agachado
     }
 
     // Update is called once per frame
@@ -46,29 +72,23 @@ public class PlayerMovement : MonoBehaviour
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); // Comprobamos si estamos tocando el suelo mediante un raycast hacia abajo
 
-        MyInput(); 
+        MyInput();
+        SpeedControl();
+        StateHandler();
 
         if(grounded)
         {
-            rb.linearDamping = groundDrag; // Si estamos en el suelo, aplicamos la fricci�n
+            rb.linearDamping = groundDrag; // Si estamos en el suelo, aplicamos la friccion
         }
         else
         {
-            rb.linearDamping = 0; // Si estamos en el aire, no aplicamos fricci�n
-        }
-
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            diegoIndex++;
-            diegoIndex %= weapons.Length;
-            sword.GetComponent<MeshFilter>().mesh = weapons[diegoIndex].mesh;
-            sword.GetComponent<MeshRenderer>().material = weapons[diegoIndex].material;
+            rb.linearDamping = 0; // Si estamos en el aire, no aplicamos friccion
         }
     }
 
     void FixedUpdate()
     {
-        MovePlayer(); // Llama a la funci�n MovePlayer para mover al jugador
+        MovePlayer(); // Llama a la funcion MovePlayer para mover al jugador
     }
 
 
@@ -76,13 +96,70 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal"); 
         verticalInput = Input.GetAxisRaw("Vertical");
+        jumping = Input.GetButton("Jump");
+        sprint = Input.GetButton("Fire3");
+        crouch = Input.GetButton("Crouch");
+
+
+        if(Input.GetButton("Jump") && readyToJump && grounded) // Si se presiona la tecla de salto, estamos listos para saltar y estamos en el suelo
+        {
+            readyToJump = false; // Cambia el estado a no listo para saltar
+            Jump(); // Llama a la funcion Jump para saltar
+            Invoke(nameof(ResetJump), jumpCooldown); // Reinicia el estado de salto después de un tiempo de espera
+        }
+
+        if (Input.GetButtonDown("Crouch") && grounded)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z); // Cambia la escala Y del objeto a la escala de agachado
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse); // Ajuste para que al agacharse no estemos flotando, añadiendo una fuerza hacia abajo al Rigidbody
+        }
+
+        if(Input.GetButtonUp("Crouch") && grounded)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z); // Cambia la escala Y del objeto a la escala normal
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse); // Ajuste para que al levantarse no estemos flotando, añadiendo una fuerza hacia arriba al Rigidbody
+        }
+
+        
+    }
+
+    private void StateHandler()
+    {
+        if(grounded && Input.GetButton("Crouch")) // Si se presiona la tecla de agachado y estamos en el suelo
+        {
+            state = MovementState.crouching; // Cambia el estado a crouching
+            speed = crouchSpeed; // Establece la velocidad al valor de velocidad de agachado
+
+            Debug.Log("Crouching"); // Imprime en la consola que estamos agachados
+        }
+        else if (grounded && Input.GetButton("Fire3")) // Si estamos en el suelo y se presiona la tecla de sprint
+        {
+            state = MovementState.sprinting; // Cambia el estado a sprinting
+            speed = sprintSpeed; // Establece la velocidad al valor de velocidad de sprint
+        }
+        else if(grounded) // Si estamos en el suelo
+        {
+            state = MovementState.walking; // Cambia el estado a walking
+            speed = walkSpeed; // Establece la velocidad al valor de velocidad de caminar
+        }
+        else if(!grounded) // Si no estamos en el suelo
+        {
+            state = MovementState.air; // Cambia el estado a air
+        }
     }
 
     private void MovePlayer()
     {
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; // Calcula la direcci�n de movimiento en funci�n de la orientaci�n y la entrada del usuario, asi siempre caminamos hacia donde estamos mirando.
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; // Calcula la direccion de movimiento en funcion de la orientacion y la entrada del usuario, asi siempre caminamos hacia donde estamos mirando.
 
-        rb.AddForce(moveDirection.normalized * speed *10f , ForceMode.Force); // Aplica una fuerza al Rigidbody en la direcci�n de movimiento normalizada, multiplicada por la velocidad y un factor de 10 para aumentar la fuerza aplicada
+        if(grounded) // Si estamos en el suelo
+        {
+            rb.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force); // Aplica una fuerza al Rigidbody en la direcci�n de movimiento normalizada, multiplicada por la velocidad y un factor de 10 para aumentar la fuerza aplicada
+        }
+        else if(!grounded) // Si no estamos en el suelo
+        {
+            rb.AddForce(moveDirection.normalized * speed * 10f * airMultiplier, ForceMode.Force); // Aplica una fuerza al Rigidbody en la direcci�n de movimiento normalizada, multiplicada por la velocidad y un factor de 10 para aumentar la fuerza aplicada
+        }
     }
 
     private void SpeedControl()
@@ -94,5 +171,17 @@ public class PlayerMovement : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized * speed;
             rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z); // Reinicia la velocidad vertical del Rigidbody a 0 antes de aplicar la fuerza de salto
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); // Aplica una fuerza de salto al Rigidbody en la dirección hacia arriba del objeto actual, "Impulse" por que se aplica una vez.
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true; // Reinicia el estado de salto, permitiendo que el jugador salte nuevamente
     }
 }
