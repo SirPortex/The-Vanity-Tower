6708c1babdaaaab5d9bc4c6518365afc0c931577
Pavel.K144 <portex144@gmail.com>
2025-05-05 19:13:31 +0200

    FixesAndNewShaders

--- Assets/Scripts/PlayerLeftArm.cs
+++ Assets/Scripts/PlayerLeftArm.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerLeftArm : MonoBehaviour
 {
     public PlayerMovement playerMovement;
-    Animator animatorLeftArm;
 
     [Header("Pushing")]
 
     public bool pushing;
 
-    
+    [Header("Emotes")]
 
+    public bool isEmoting;
+    public int emoteIndex;
+    public bool downCross;
+
+    bool emote;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,7 +23,6 @@ public class PlayerLeftArm : MonoBehaviour
     {
         //playerMovement.GetComponent<PlayerMovement>();
         playerMovement= GetComponentInParent<PlayerMovement>();
-        animatorLeftArm = playerMovement.animator;
     }
 
     // Update is called once per frame
@@ -26,25 +30,135 @@ public class PlayerLeftArm : MonoBehaviour
     {
         pushing = Physics.Raycast(transform.position, playerMovement.orientation.forward, out RaycastHit hit, 1f, playerMovement.whatIsGround);
 
+
         LeftArmInput();
         Push();
+        LeftArmGamepad();
     }
 
-    private void LeftArmInput()
+    public void LeftArmInput()
     {
-        
+        emote = Input.GetButtonDown("Emote");
+
+        if (emote && !isEmoting)
+        {
+            if(!playerMovement.isAttaking || !playerMovement.isBlocking)
+            {
+                
+                EmoteHandler();
+            }
+
+        }
+    }
+
+    public void LeftArmGamepad()
+    {
+        if (playerMovement.gamepad != null)
+        {
+            downCross = playerMovement.gamepad.dpad.down.IsPressed();
+
+            if (downCross && !isEmoting)
+            {
+                if (!playerMovement.isAttaking || !playerMovement.isBlocking)
+                {
+                    EmoteHandler();
+                }
+            }
+        }
     }
 
     public void Push()
     {
         if (pushing)
         {
+            playerMovement.animator.SetBool("IsAttacking01", false);
+            playerMovement.animator.SetBool("IsAttacking02", false);
+            playerMovement.animator.SetBool("IsBlocking", false);
+            isEmoting = true;
             playerMovement.animator.SetBool("IsPushing", true);
         }
         else
         {
+            isEmoting = false;
             playerMovement.animator.SetBool("IsPushing", false);
         }
+    }
+
+    public void EmoteHandler()
+    {
+        isEmoting = true;
+        emoteIndex = Random.Range(1, 7);
+        switch (emoteIndex)
+        {
+            case 1:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote1", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+            case 2:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote2", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+            case 3:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote3", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+            case 4:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote4", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+            case 5:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote5", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+            case 6:
+
+                CancelRightArm();
+                playerMovement.animator.SetBool("Emote6", true);
+                Invoke("StopEmote", 3f);
+
+                break;
+        }
+
+    }
+
+    public void StopEmote()
+    {
+        ActivateRightArm();
+        emoteIndex = 0;
+        isEmoting = false;
+        playerMovement.animator.SetBool("Emote1", false);
+        playerMovement.animator.SetBool("Emote2", false);
+        playerMovement.animator.SetBool("Emote3", false);
+        playerMovement.animator.SetBool("Emote4", false);
+        playerMovement.animator.SetBool("Emote5", false);
+        playerMovement.animator.SetBool("Emote6", false);
+    }
+
+    public void CancelRightArm()
+    {
+        playerMovement.isBlocking = true;
+        playerMovement.isAttaking = true;
+    }
+
+    public void ActivateRightArm()
+    {
+        playerMovement.isBlocking = false;
+        playerMovement.isAttaking = false;
     }
 
     private void OnDrawGizmos()
